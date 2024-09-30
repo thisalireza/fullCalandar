dfe5f4d3df45c9a3f1de814e9c002f9046ca56bd
Alireza <thisisaalireza@gmail.com>
2024-09-30 22:50:03 +0330

    Add project files.

---
+++ Context/MyContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace fullCalandar.Context
+{
+    public class MyContext:DbContext
+    {
+        public MyContext(DbContextOptions<MyContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
