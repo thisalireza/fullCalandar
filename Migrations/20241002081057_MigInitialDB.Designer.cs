dc6e171e3f6c6d20986759a96b1bd17c89c461be
Alireza <thisisaalireza@gmail.com>
2024-10-02 11:48:46 +0330

    fix data source in connectionString

--- Migrations/20241002081057_MigInitialDB.Designer.cs
+++ Migrations/20241002081057_MigInitialDB.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using fullCalandar.Context;
 
@@ -10,9 +11,11 @@ using fullCalandar.Context;
 namespace fullCalandar.Migrations
 {
     [DbContext(typeof(MyContext))]
-    partial class MyContextModelSnapshot : ModelSnapshot
+    [Migration("20241002081057_MigInitialDB")]
+    partial class MigInitialDB
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20241002081057_MigInitialDB.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace fullCalandar.Migrations
+{
+    /// <inheritdoc />
+    public partial class MigInitialDB : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CalandarInfos",
+                columns: table => new
+                {
+                    CalendarInfoId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<int>(type: "int", nullable: false),
+                    EventTitle = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    EventDescription = table.Column<string>(type: "nvarchar(350)", maxLength: 350, nullable: true),
+                    StartDate = table.Column<string>(type: "nvarchar(10)", maxLength: 10, nullable: false),
+                    EndDate = table.Column<string>(type: "nvarchar(10)", maxLength: 10, nullable: false),
+                    StartTime = table.Column<string>(type: "nvarchar(10)", maxLength: 10, nullable: false),
+                    EndTime = table.Column<string>(type: "nvarchar(10)", maxLength: 10, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CalandarInfos", x => x.CalendarInfoId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CalandarInfos");
+        }
+    }
+}
